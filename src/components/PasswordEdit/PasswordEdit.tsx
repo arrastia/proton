ef0314f57e38b5f7e368f52a6a23ed744048c71e
Arrastia <arrastia@pm.me>
2022-08-10 19:35:17 +0200

    FIX - multiple components & react-key error
    UPDATE - convert file to tsx (no proper types)

---
+++ src/components/PasswordEdit/PasswordEdit.tsx
@@ -0,0 +1,104 @@
+import { useCallback, useState } from 'react';
+
+import classes from './PasswordEdit.module.css';
+
+import { UrlList } from './components/UrlList/UrlList';
+import Button from 'atoms/Button';
+import Icon from 'atoms/Icon';
+import Input from 'atoms/Input';
+import Labelled from 'atoms/Labelled';
+import LabelledIconButton from 'components/LabelledIconButton';
+import TextArea from 'atoms/TextArea';
+
+function PasswordEdit({ onCancel, onDelete, onSave, password }: any) {
+  const [values, setValues] = useState(password);
+
+  const [urlInput, setUrlInput] = useState('');
+
+  function change(partial: any) {
+    setValues((values: any) => ({ ...values, ...partial }));
+  }
+
+  function handleChange(e: any) {
+    change({ [e.target.name]: e.target.value });
+  }
+
+  function handleSaveClick() {
+    onSave({ ...password, ...values });
+  }
+
+  function handleDeleteClick() {
+    onDelete();
+  }
+
+  function handleCancelClick() {
+    onCancel();
+  }
+
+  function handleUrlAdd() {
+    const urls = values.url || [];
+
+    urls.unshift(urlInput);
+
+    change({ url: urls });
+
+    setUrlInput('');
+  }
+
+  const handleUrlDelete = useCallback(
+    (index: any) => () => {
+      const urls = values.url || [];
+
+      urls.splice(index, 1);
+
+      change({ url: urls });
+    },
+    []
+  );
+
+  return (
+    <div className={classes.container}>
+      <h2 className={classes.title}>
+        <input autoFocus className={classes.titleInput} name="name" onChange={handleChange} value={values.name} />
+      </h2>
+      <div className={classes.content}>
+        <Labelled label="description">
+          <TextArea name="description" onChange={handleChange} value={values.description} />
+        </Labelled>
+
+        <Labelled label="value">
+          <Input name="value" onChange={handleChange} value={values.value} />
+        </Labelled>
+
+        <Labelled label="url">
+          <div>
+            <Input onChange={e => setUrlInput(e.target.value)} style={{ marginRight: 4 }} value={urlInput} />
+
+            <Button onClick={handleUrlAdd}>Add</Button>
+          </div>
+
+          <UrlList onDelete={handleUrlDelete} urls={values.url} />
+        </Labelled>
+      </div>
+      <div className={classes.controls}>
+        <LabelledIconButton
+          className={classes.cancel}
+          icon={<Icon className="fas fa-times" size="small" />}
+          label="Cancel"
+          onClick={handleCancelClick}
+        />
+
+        <LabelledIconButton icon={<Icon className="fas fa-save" size="small" />} label="Save" onClick={handleSaveClick} />
+
+        <LabelledIconButton
+          className={classes.delete}
+          icon={<Icon className="fas fa-trash" size="small" />}
+          label="Delete"
+          onClick={handleDeleteClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PasswordEdit;
---
+++ src/components/PasswordEdit/components/UrlList/UrlList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import clsx from 'clsx';
+
+import classes from '../../PasswordEdit.module.css';
+
+import Icon from 'atoms/Icon';
+import List from 'atoms/List';
+import ListItem from 'atoms/ListItem';
+
+type Props = {
+  onDelete: any;
+  urls: any;
+};
+
+export const UrlList = React.memo(({ urls, onDelete }: Props) => {
+  return (
+    <List className={classes.urlList}>
+      {urls?.map((urlEntry: any, index: number) => (
+        <ListItem className={classes.urlListItem} dense key={urlEntry}>
+          <input autoFocus value={urlEntry} />
+          <Icon onClick={() => onDelete(index)} size="small" className="fas fa-times" />
+        </ListItem>
+      ))}
+      {urls?.length === 0 && (
+        <ListItem className={clsx(classes.urlListItem, classes.urlListItemEmpty)} dense>
+          No urls added
+        </ListItem>
+      )}
+    </List>
+  );
+});
