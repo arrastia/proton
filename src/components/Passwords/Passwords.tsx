4becc6f7eb65330b6ba03e1b7dcdfc9c01d306c1
Arrastia <arrastia@pm.me>
2022-08-14 14:40:21 +0200

    UPDATE - Passwords component view

---
+++ src/components/Passwords/Passwords.tsx
@@ -0,0 +1,20 @@
+import { useRecoilValue } from 'recoil';
+
+import classes from './Passwords.module.css';
+
+import List from 'atoms/List';
+import { Password } from './components/Password';
+
+import { allPasswordsState } from 'stores/PasswordStore';
+
+import type { Password as TPass } from 'models';
+
+export const Passwords = () => {
+  const passwords = useRecoilValue(allPasswordsState);
+
+  console.log('passwords :>> ', passwords);
+
+  const renderListItem = (password: TPass) => <Password key={password.id} {...password} isVulnerable={false} />;
+
+  return <List className={classes.passwords}>{passwords.map(renderListItem)}</List>;
+};
---
+++ src/components/Passwords/components/Password/Password.tsx
@@ -0,0 +1,19 @@
+import classes from './Password.module.css';
+
+import Icon from 'atoms/Icon';
+import ListItem from 'atoms/ListItem';
+
+import type { Password as TP } from 'models';
+
+interface PasswordProps extends TP {
+  isVulnerable: boolean;
+}
+
+export const Password = ({ name, isVulnerable }: PasswordProps) => {
+  return (
+    <ListItem className={classes.listItem} clickable>
+      {name}
+      {isVulnerable && <Icon className="fas fa-exclamation-triangle" size="small" />}
+    </ListItem>
+  );
+};
